a64f6116e8a8a87b18cead6055bfffcbb76d01fc
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-04-30 03:09:04 +0800

    :zap: Added Hero Banner component and MainContent component in Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import { HeroBanner } from "./Main/Hero Banner/HeroBanner";
+import MainContent from "./Main/MainContent/MainContent";
+
+
+
+export default function Main() {
+    
+
+    return (
+        <>
+        <HeroBanner />
+        <MainContent />
+        </>
+    )
+}
