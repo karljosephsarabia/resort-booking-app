42a2390d072c2d734e818848793b9f1200aec06e
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-05-04 10:34:07 +0800

    :zap: import components in landingpage.js

---
+++ src/components/MainComponent/LandingPage.js
@@ -0,0 +1,11 @@
+import HeroBanner from "./LandingPage/Hero Banner/HeroBanner";
+import MainContent from "./LandingPage/MainContent/MainContent";
+
+export default function LandingPage() {
+    return (
+        <>
+            <HeroBanner />
+            <MainContent />
+        </>
+    );
+}
