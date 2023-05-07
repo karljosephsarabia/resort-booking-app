df10a29e7120202811d47dadd2c8e13b531c9028
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-05-07 15:31:25 +0800

    :zap: implement persist reducer and clear search state in store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "@reduxjs/toolkit";
-import responsiveSlice, { resortListSlice } from "./ResponsiveSlice";
+import responsiveSlice, { resortListSlice, searchSlice, bookSlice } from "./ResponsiveSlice";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
     responsive: responsiveSlice.reducer,
-    resortList: resortListSlice.reducer
+    resortList: resortListSlice.reducer,
+    search: searchSlice.reducer,
+    book: bookSlice.reducer
     //add reducers
 });
 
-const store = configureStore({
-    reducer: rootReducer
+function clearReducer(state, action) {
+    if (action.type === 'CLEAR_SEARCH_STATE') {
+        state = {
+            ...state,
+            book: bookSlice.initialState
+        };
+    }
+    return rootReducer(state, action);
+}
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['responsive', 'resortList', 'search']
+};
+
+const persistedReducer = persistReducer(persistConfig, clearReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer
 });
 
-export default store;
+export const persistor = persistStore(store);
