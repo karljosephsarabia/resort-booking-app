7c3e21d8ac8fa984478a4c08cd2436eac6214d88
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-05-07 15:17:24 +0800

    :pencil2: Change hero banner subtitle to "Travel and Explore" heroBannerDesktop.js

--- src/components/MainComponent/LandingPage/Hero Banner/HeroBannerDesktop.js
+++ src/components/MainComponent/LandingPage/Hero Banner/HeroBannerDesktop.js
@@ -7,7 +7,7 @@ export default function HeroBannerDesktop() {
         <>
             <Row>
                 <Col className="d-flex flex-column align-items-center">
-                    <h1 className="hero-banner-name-desktop position-relative">Dora<span className="hero-banner-text-desktop">Explore and Travel</span></h1>
+                    <h1 className="hero-banner-name-desktop position-relative">Dora<span className="hero-banner-text-desktop">Travel and Explore</span></h1>
                 </Col>
             </Row>
         </>
