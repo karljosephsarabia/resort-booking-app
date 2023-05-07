2fa18b05c73e2a226f8604e8e018905c7a3386ba
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-05-07 15:09:45 +0800

    :zap: import persist redux and add components in index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './store/store'
+import { store } from './store/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/store.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor} >
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
