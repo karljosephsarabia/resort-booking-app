558e251a2926347394d8d1e256ffc6a25fe9cfd5
Gesila Lobiano <baringgesil@gmail.com>
2023-05-09 10:35:17 +0800

    fixing About image and deleting images

--- src/components/MainComponent/AboutUsPage/AboutDeveloper.js
+++ src/components/MainComponent/AboutUsPage/AboutDeveloper.js
@@ -15,7 +15,7 @@ function Developer() {
                         <p>Leisure destination providing a compelling mix of luxury relaxation and wellness, lively entertainment, and exciting recreational activities including captivating child-friendly facilities.
                             Guests can savour beach-side luxe within its spacious guest rooms and suites, along with enticing wining and dining options.
                         </p>
-                        <img src={Victor} alt="resorts" id='about-photo' />
+                        {/* <img src={Victor} alt="resorts" id='about-ph' /> */}
                     </div>
                 </div>
             </div>
