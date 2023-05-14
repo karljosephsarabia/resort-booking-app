cc3b8e094e8a5fcf01f8cc880194f9e11b526484
Karl Joseph Sarabia <sarabia.karljoseph@gmail.com>
2023-05-14 18:48:31 +0800

    :zap: revise properties of user login inputfield.js and radio.js

--- src/components/LoginRegisterComponents/InputField.js
+++ src/components/LoginRegisterComponents/InputField.js
@@ -1,9 +1,10 @@
 function InputField(props) {
+
     return(
         <>
             <div className="mt-2">
                 <label htmlFor={props.id} className="form-label form-text mt-1" >{props.label}</label>
-                <input type={props.input} className="form-control label-text mt-1" id={props.id} ></input>
+                <input type={props.input} className="form-control label-text mt-1" id={props.id} name={props.name} value={props.value} onChange={props.onChange}></input>
             </div>
         </>
     );
--- src/components/LoginRegisterComponents/Radio.js
+++ src/components/LoginRegisterComponents/Radio.js
@@ -2,8 +2,8 @@ function Radio(props){
     return(
         <>
             <div className="form-check">
-                <input className="form-check-input" type="radio" name="register-as" id={props.id}/>
-                <label className="form-check-label " for={props.id}>
+                <input className="form-check-input" type="radio" name={props.name} id={props.id} value={props.value} checked={props.checked} onChange={props.onChange}/>
+                <label className="form-check-label " for={props.value}>
                     <p className="radio-text">{props.labeltext}</p>
                 </label>
             </div>
