4173d3b54a5f33b7c58095560631a1ee17e29a16
Ierie Marie Manguit <empressre14@gmail.com>
2023-05-14 16:06:10 +0800

    Fixed main footer alignments

--- src/components/MainComponent/MainFooter/MainFooter.js
+++ src/components/MainComponent/MainFooter/MainFooter.js
@@ -9,30 +9,33 @@ import YouTube from './images/youtube.png';
 const MainFooter = () => {
     return(
         <footer className='TheFooter p-3'>
+            <div>
+                <p className='ctafooter lead d-flex justify-content-center fw-bold'>Follow Us For More Destinations</p>
+            </div>
             <div className='SocialMediaIcons'>
                     <div className='d-flex justify-content-center'>
                         <a href='https://www.facebook.com/' target='_blank' rel="noreferrer">
-                            <img className='smicon' src={Facebook} alt='Facebook'/>
+                            <img className='smicon m-3' src={Facebook} alt='Facebook'/>
                         </a>
 
                         <a href='https://www.instagram.com/' target='_blank' rel="noreferrer">
-                            <img className='smicon' src={Instagram} alt='Instagram'/>
+                            <img className='smicon m-3' src={Instagram} alt='Instagram'/>
                         </a>
 
                         <a href='https://www.tiktok.com/' target='_blank' rel="noreferrer">
-                            <img className='smicon' src={Tiktok} alt='Tiktok'/>
+                            <img className='smicon m-3' src={Tiktok} alt='ITiktok'/>
                         </a>
 
                         <a href='https://twitter.com/' target='_blank' rel="noreferrer">
-                            <img className='smicon' src={Twitter} alt='Twitter'/>
+                            <img className='smicon m-3' src={Twitter} alt='Twitter'/>
                         </a>
 
                         <a href='https://youtube.com/' target='_blank' rel="noreferrer">
-                            <img className='smicon' src={YouTube} alt='YouTube'/>
+                            <img className='smicon m-3' src={YouTube} alt='YouTube'/>
                         </a>
                     </div>
             </div>
-            <div className='CopyrightText d-flex justify-content-center'>
+            <div className='CopyrightText d-flex justify-content-center mt-3'>
             © 2023 Tropicana Getaway All Rights Reserved.
             </div>
         </footer>
