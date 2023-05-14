195a45aa7a9865219c3a3e511b8cd3e843c28aa2
Ierie Marie Manguit <empressre14@gmail.com>
2023-05-14 16:26:37 +0800

    Added footer component across pages

--- src/components/MainComponent/AboutUsPage/About.js
+++ src/components/MainComponent/AboutUsPage/About.js
@@ -3,7 +3,6 @@ import AboutUsTitle from './AboutUsTitle';
 import OurStoryContent from './OurStoryContent';
 import Developer from './AboutDeveloper';
 import TheTeamHeading from './TheTeamHeading';
-import MainFooter from '../MainFooter/MainFooter';
 import './About.css';
 
 
@@ -16,7 +15,6 @@ export default function About() {
       <OurStoryContent />
       <TheTeamHeading />
       <Developer/>
-      <MainFooter />
     </div> 
     </>
   )
